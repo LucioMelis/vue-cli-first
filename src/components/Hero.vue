6929fdf82eb88edeaee6678dc97b2bae078a7b2a
luciomelis <luciomelis1992@gmail.com>
2022-04-05 19:19:24 +0200

    esercizio terminato

---
+++ src/components/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <img src="../assets/logotrono.png" alt="logoTrono" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeroImg",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+img {
+  width: 100px;
+}
+</style>
